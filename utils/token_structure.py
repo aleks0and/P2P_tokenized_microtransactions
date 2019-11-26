f61649718ee48bef068dcc17be76c389f44f0777
aleks0and <aleksander.kusmierczyk@gmail.com>
2019-11-26 11:20:08 +0100

    setting up token structure

--- utils/token_structure.py
+++ utils/token_structure.py
@@ -1,9 +1,32 @@
 # structure of tokens defined
 
+# @todo: definition of token types
+# @todo: verify variable types
 
 class SMTM_token:
-
-    def __init__(self) -> None:
+    def __init__(self, name, surname, account_name, balance, email_address,
+                 status, token_type, date_in, date_out) -> None:
+        self.name = name
+        self.surname = surname
+        # can be nullable but we want to keep it as string?
+        if account_name == None:
+            self.account_name = 'None'
+        else:
+            self.account_name = account_name
+        if type(balance) == int:
+            self.balance = balance
+        else:
+            # assign different token type
+            self.balance = balance
+        self.email_address = email_address
+        # 0 / 1 paid - pending
+        self.status = status
+        # from predefined list.
+        self.token_type = token_type
+        # datetime format
+        self.date_in = date_in
+        # date of completion None in other times.
+        self.date_out = date_out
         super().__init__()
 
     def __str__(self) -> str:
